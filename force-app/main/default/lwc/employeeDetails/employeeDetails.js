4e6aaed376c49c6c65668ae37952d93f37059a82
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-04-09 15:10:36 +0530

    Adding iterator to simpleCalculator, adding empdtl

--- force-app/main/default/lwc/employeeDetails/employeeDetails.js
+++ force-app/main/default/lwc/employeeDetails/employeeDetails.js
@@ -1,3 +1,32 @@
 import { LightningElement } from 'lwc';
 
-export default class EmployeeDetails extends LightningElement {}
+export default class EmployeeDetails extends LightningElement {
+    name;
+    phone;
+    mail;
+    ctc;
+    isShown = false;
+    isSelected = false;
+
+    handleName(event) {
+        this.name = event.target.value;
+    }
+    handlePhone(event) {
+        this.phone = event.target.value;
+    }
+    handleMail(event) {
+        this.mail = event.target.value;
+    }
+    handleCtc(event) {
+        this.ctc = event.target.value;
+    }
+    handleClick() {
+        this.isSelected = !this.isSelected;
+        showDetails(this.isSelected);
+    }
+    showDetails(isSelected) {
+        if(isSelected) {
+            this.isShown = true;
+        }
+    }
+}
