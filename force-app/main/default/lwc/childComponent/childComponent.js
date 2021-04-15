9bbddc723673d96cab6e677c1541f4f12712853b
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-04-15 11:45:30 +0530

    child/parent Component, child to parent comm

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,5 +1,14 @@
 import { api, LightningElement } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
-
+    handlePrevious() {
+        console.log('Previous is Clicked');
+        const prevEvent = new CustomEvent('previous');
+        this.dispatchEvent(prevEvent);
+    }
+    handleNext() {
+        console.log('Next is clicked');
+        const nextEvent = new CustomEvent('next');
+        this.dispatchEvent(nextEvent);
+    }
 }
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class ParentComponent extends LightningElement {}
+export default class ParentComponent extends LightningElement {
+    pageNumber = 1;
+
+    handlePrevious() {
+        if (this.pageNumber > 1) {
+            this.pageNumber -= 1;
+        }
+    }
+    handleNext() {
+        if (this.pageNumber < 10) {
+            this.pageNumber += 1;
+        }
+    }
+}
